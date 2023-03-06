6850fcfbe0c3110c9d6feacb618e29454f93fcec
Erlend Lemva Ousdal <erlendou@uio.no>
2023-03-06 14:45:58 +0100

    'numerisk'

--- 1D_MC.py
+++ 1D_MC.py
@@ -4,7 +4,14 @@ from random import seed, random
 
 #analytical expression for the wavefunction
 def waveFunction(alpha, betha, r):
-    
+    wF = 1
+    for i in range(len(r)):
+        r2 = 0
+        for j in range(len(r[0])):
+            r2 += r[i,j]**2
+        wF *= np.exp(-alpha*r2)
+    return(wF)    
+    """
     if(len(r[0]) == 1):
         x = 0
         for i in range(len(r)):
@@ -20,21 +27,30 @@ def waveFunction(alpha, betha, r):
     
     if(len(r[0]) == 3):
         return(np.exp(-alpha*(r[0][0]**2+r[0][1]**2+r[0][2]**2)))
-
+    """
 #analytical expression for the local energy of one boson in 1D     
 def localEnergy(alpha, betha, r):
-    if(len(r[0]) == 1):
-        x = 0
-        for i in range(len(r)):
-            x += r[i][0]**2
-        return(len(r[0])*alpha+0.5*(1-4*alpha**2)*x)
-    
-    if(len(r[0]) == 2):
-          
-        x = r[0][0]**2
-        y = r[0][1]**2
-        return((2*alpha-4*alpha*alpha*(x+y)+0.5*(x+y))/len(r[0]))
+    E = 0
+    for i in range(len(r)):
+        r2 = 0
+        for j in range(len(r[0])):
+            r2 += r[i,j]**2
+        E+=(len(r[0])*alpha-2*alpha**2*r2+0.5*r2)    
+    return(E/len(r))        
 
+def d2 (alpha, betha, r, dx):
+    return((waveFunction(alpha,betha,r+dx) - 2*waveFunction(alpha, betha, r) + waveFunction(alpha, betha, r-dx))/(dx**2))
+
+def localEnergy_num(alpha, betha, r):
+    E = 0
+    for i in range(len(r)):
+        r2 = 0
+        for j in range(len(r[0])):
+            r2+=r[i,j]**2
+        
+        E+=0.5*(-d2(alpha, betha, r, 0.001)+r2)
+    return E/len(r)  
+        
 #montecarlo cycling starts here, using metropolis-algorithm without importance sampling 
 def monteCarlo_metropolis(N, alpha, betha, maxVar, nParticle, dim):
     
@@ -71,7 +87,7 @@ def monteCarlo_metropolis(N, alpha, betha, maxVar, nParticle, dim):
                 if(random() < wfNew**2/wfOld**2):
                     posOld = posNew.copy()
                     wfOld = wfNew
-                    dE = localEnergy(alpha,betha, posOld)
+                    dE = localEnergy_num(alpha,betha, posOld)
                 energy += dE
                 energy2 += dE**2
                 
@@ -82,17 +98,18 @@ def monteCarlo_metropolis(N, alpha, betha, maxVar, nParticle, dim):
             Vars[i,j] = var    
     return(Energies, alphaList, Vars)
 
-    
+  
 #main
 
 N = 1E4
-alpha = 0.6
+alpha = 0.3
 betha = 1.0
-maxVar = 20
+maxVar = 40
 nParticle = 1
-dim = 2 
+dim = 1
 
 Energies, alphaList, Vars = monteCarlo_metropolis(N, alpha, betha, maxVar, nParticle, dim)
+
 
 plt.plot(alphaList,Energies)
 plt.xlabel('Alpha (AU)')
@@ -101,5 +118,5 @@ plt.show()
 plt.close()
 plt.plot(alphaList,Vars)
 plt.xlabel('Alpha (AU)')
-plt.ylabel('Energy (hw)')
+plt.ylabel('Variance (AU)')
 plt.show()
